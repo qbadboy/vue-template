a302f8e45cfb2b102b693750f86c31892bcba7b9
zhuhong <qbadboy@gmail.com>
2020-07-03 00:23:57 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to {{project}}"/>
+    <HelloWorld msg="Hello {{ project }}"/>
   </div>
 </template>
 
