9c7bccc4cff769b9951eea67df51e7f3fd147ff2
zhuhong <qbadboy@gmail.com>
2020-07-03 16:12:12 +0800

    fix bug

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,3 +1,36 @@
 <template>
+  <div>
     <router-link class="link" to=""></router-link>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Link',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.link{
+  margin: 0 10px;
+}
+</style>
